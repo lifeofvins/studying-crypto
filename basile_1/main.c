8eb5a4eab9d6ddab218aa5132270f03813651b0a
lifeofvins <vincenzo.sagristano@studenti.polito.it>
2022-10-25 19:30:21 +0200

    bas_1

---
+++ basile_1/main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include </usr/include/openssl/evp.h>
+#include </usr/include/stdlib.h>
+#include <string.h>
+
+#define ENC 1
+#define DEC 0
+
+#define BUFFER_SIZE 1024
+
+int main(int argc, char const *argv[])
+{
+    unsigned char ibuf[BUFFER_SIZE], obuf[BUFFER_SIZE];
+    int key_size, ilen, olen, tlen;
+    int i;
+
+    // key
+    unsigned char *key = (unsigned char *)"0123456789012345";
+    // iv
+    unsigned char *iv = (unsigned char *)"aaaaaaaaaaaaaaaaa";
+
+    // print the key and the 
+    // initialization vector
+    // as hexadecimal values.
+    printf("key: ");
+    for (i = 0; i < 16; i++)
+        printf("%02x", key[i]);
+    printf("\n");
+    printf("iv: ");
+    for (i = 0; i < 16; i++)
+        printf("%02x", iv[i]);
+    printf("\n");
+
+    // key generation
+    key_size = EVP_CIPHER_key_length(EVP_aes_128_cbc());
+    printf("key_size = %d\n", key_size);
+
+    // Creation of the object for the context
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    EVP_CIPHER_CTX_init(ctx);
+    EVP_CipherInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv, ENC);
+
+    unsigned char *message = (unsigned char *)"this is the message";
+    printf("plain message: ");
+    for (i = 0; i < 16; i++)
+        printf("%02x", obuf[i]);
+    printf("\n");
+
+    // Excecution of the algorithm
+    int tot = 0;
+    EVP_CipherUpdate(ctx, obuf, &olen, message, strlen(message));
+    tot += olen;
+
+    // Finalization of the process
+    EVP_CipherFinal_ex(ctx, obuf+tot, &tlen);
+    tot += tlen;
+
+    printf("encrypted message: ");
+    for (i = 0; i < 16; i++)
+        printf("%02x", obuf[i]);
+    printf("\n");
+
+    // destruction of the objext
+    EVP_CIPHER_CTX_free(ctx);
+
+    return 0;
+}
